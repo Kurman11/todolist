0595701f3512225471b3cbd7c97bee72f667fe4c
Kurman11 <skedyd45@naver.com>
2024-12-13 11:21:25 +0900

    useMemo

--- todolist/src/components/Lists.jsx
+++ todolist/src/components/Lists.jsx
@@ -1,5 +1,5 @@
 import TodoItem from './TodoItem'
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 export default function Lists({todos, onUpdate, onDelete}){
   const [search,setSearch] = useState("");
@@ -17,9 +17,25 @@ export default function Lists({todos, onUpdate, onDelete}){
 
   const filteredTodos = getFilteredData();
 
+  const {totalCount, doneCount, notDoneCount } = 
+  useMemo(()=>{
+    const totalCount = todos.length;
+    const doneCount = todos.filter(
+      (todo) => todo.isDone).length;
+      const notDoneCount = totalCount - doneCount;
+      return {
+        totalCount,
+        doneCount,
+        notDoneCount
+      }
+  },[todos])
+  // const {totalCount, doneCount, notDoneCount} = getAnalyzedDate()
   return (
     <div className='flex flex-col gap-5'>
       <h3>Todo List ✔</h3>
+      <div>total : {totalCount} </div>
+      <div>done : {doneCount} </div>
+      <div>notDone : {notDoneCount} </div>
       <input value={search} onChange={onChangeSearch} className='w-full border-b-2 border-slate-300 py-3.5 outline-none focus:border-sky-300 ' type="text" placeholder="검색어를 입력하세요" />
       <div className='flex flex-col gap-5'>
         {filteredTodos.map((todo)=>{
